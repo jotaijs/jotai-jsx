d8f13db4e7dec9a33e4166b6e0423a2baca2cdd8
daishi <daishi@axlight.com>
2021-08-09 12:10:59 +0900

    allow useState for lazy initialization

--- src/compat/useState.ts
+++ src/compat/useState.ts
@@ -1,7 +1,13 @@
 import { atom } from 'jotai';
 import { useAtom, useConstant } from '../index';
 
-export function useState<Value>(initialValue: Value) {
-  const stateAtom = useConstant(() => atom(initialValue));
+export function useState<Value>(initialValue: Value | (() => Value)) {
+  const stateAtom = useConstant(() =>
+    atom(
+      typeof initialValue === 'function'
+        ? (initialValue as () => Value)()
+        : initialValue,
+    ),
+  );
   return useAtom(stateAtom);
 }
